432222af3065a4304d3ceb41ba8c4aaf11006d3a
Your Name <you@example.com>
2023-07-06 14:03:49 +0200

    author section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
 document.querySelectorAll('.form__tab').forEach(button => {
   button.addEventListener('click', () => {
-    // Usuń klasę active z aktualnie aktywnego przycisku
+
     document.querySelector('.form__tab.active').classList.remove('active');
-
-    // Dodaj klasę active do klikniętego przycisku
     button.classList.add('active');
-
-    // Ukryj aktualnie wyświetlaną treść
     document.querySelector('.tab__content.active').classList.remove('active');
-
-    // Pokaż treść związane z klikniętym przyciskiem
     const target = button.getAttribute('data-target');
     document.querySelector(`.${target}`).classList.add('active');
   });
 
-  // Pobranie elementów
   const quantitySelect = document.getElementById('quantity');
   const totalInput = document.getElementById('total');
-
-  // Dodanie funkcji do zdarzenia 'change'
   quantitySelect.addEventListener('change', function () {
-    // Obliczenie wartości
     const total = this.value * 39 + 10;
-
-    // Ustawienie wartości w inpucie
     totalInput.value = total;
   });
+
+  const links = document.querySelectorAll(".smooth-scroll");
+
+  for (const link of links) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const target = document.querySelector(link.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  }
 });
